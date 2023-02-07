8af8eb886087c981f090265f61e7a7773713dd2f
Zacah <124702897+Zacah@users.noreply.github.com>
2023-02-07 12:52:18 -0300

    opção de saida
    adição de nova funcionalidade para saida do usuario

--- Meuteste7.c
+++ Meuteste7.c
@@ -130,7 +130,8 @@ int main()
 		printf("escolha a opção desejada do menu:\n\n");
 		printf("\t1 - registrar nome\n");
 		printf("\t2 - consultar nomes\n");
-		printf("\t3 - deletar nomes\n\n\n");
+		printf("\t3 - deletar nomes\n");
+		printf("\t4 - Sair do sistema\n");
 		printf("opção:  ");//fim do menu
 	
 		scanf("%d", &opcao);//Armazenando a escolha do usuario
@@ -149,6 +150,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 		
